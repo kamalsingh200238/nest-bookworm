3c673e4ff8e1e20087196877d7686c483c84e646
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-20 23:49:52 +0530

    feat: Connect to Db with mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseConfigModule } from './mongoose/mongoose.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
