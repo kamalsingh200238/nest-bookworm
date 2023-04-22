848aba10888c72b55a82f9feef0288b822ead0ea
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-22 16:55:03 +0530

    feat: Add auth module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseConfigModule } from './mongoose/mongoose.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseConfigModule],
+  imports: [MongooseConfigModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
