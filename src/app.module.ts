2a67bfe464de7f9cff9b3bfb642365400a5b7a5a
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-23 22:51:55 +0530

    chore: Import user service in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseConfigModule } from './mongoose/mongoose.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseConfigModule, AuthModule],
+  imports: [MongooseConfigModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
