599374dcfb6d68f2205dd31ba1faa0a7d5309e3e
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-12 16:38:56 +0530

    Feat: Add simple auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseConfigModule } from './mongoose/mongoose.module';
-import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseConfigModule, AuthModule, UserModule],
+  imports: [MongooseConfigModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
