74c3a66c05bf5f16c9830b37e96e46972166ed67
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-13 23:06:14 +0530

    feat(auth): Register route functionality
    - A new function in user service that creates new user.
    - export user service from user module
    - import user service in auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 import { SignUpDto } from './dto/signUp.dto';
 
 @Injectable()
 export class AuthService {
-  signUp(singUpDto: SignUpDto) {
-    return { singUpDto };
+  constructor(private userService: UserService) {}
+  async signUp(signUpDto: SignUpDto) {
+    await this.userService.createNewUser(signUpDto);
+    return { signUpDto };
+    // add the user in the database
+    // generate jwt token
+    // set the token in the cookie
+    // return the data (only id, username, email)
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { SignUpDto } from 'src/auth/dto/signUp.dto';
+import { User } from './user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+ constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  async createNewUser(signUpDto: SignUpDto){
+    const user = new this.userModel(signUpDto)
+    return user.save()
+  }
+}
