49bcdc918853c104e9f16a162e2213355ef8c0c3
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-19 20:56:23 +0530

    feat(auth controller): signup route complete
    - First create new user
    - Then generate jwt token
    - Set the token in cookie
    - Send back the response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,33 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signUp.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('signup')
-  signUp(@Body() signUpDto: SignUpDto) {
-    return this.authService.signUp(signUpDto);
+  async signUp(@Body() signUpDto: SignUpDto, @Res() res: Response) {
+    const { username, email, password } = signUpDto;
+
+    // create new user
+    const user = await this.userService.createNewUser({
+      username,
+      email,
+      password,
+    });
+
+    // create jwt token
+    const jwtToken = await this.authService.generateJwtToken(user);
+
+    // send the cookie in jwt token
+    res.cookie('token', jwtToken);
+
+    res.send({ userId: user._id, username: user.username, email: user.email });
   }
 }
