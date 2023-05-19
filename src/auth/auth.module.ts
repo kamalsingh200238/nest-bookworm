282c527d38f516a514ee558527d026b4aa8ad719
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-19 20:56:48 +0530

    feat(auth module): import Jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports:[UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: 'this-is-secret', // TODO: add secret later
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
