bbf0c879b3531c911a2c816dfe21629fbcc99a47
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-19 20:53:33 +0530

    feat(user.service.ts): Service to add new user
    - function createNewUser first extracts username, password, email from the userInfo
    - Then it checks if the user already exists. If user already exist it throws a Bad request error
    - If user doesn't exists it will hash the password
    - Create a new user
    - Then return the user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { SignUpDto } from 'src/auth/dto/signUp.dto';
 import { User } from './user.schema';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
- constructor(@InjectModel(User.name) private userModel: Model<User>) {}
-  async createNewUser(signUpDto: SignUpDto){
-    const user = new this.userModel(signUpDto)
-    return user.save()
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  async createNewUser(userInfo: User) {
+    const { username, email, password } = userInfo;
+    // check if the user already exists
+    const existingUser = await this.userModel.findOne({ email }).exec();
+
+    if (existingUser) {
+      // if user exist then send bad request error
+      throw new BadRequestException('User already exist, use different email');
+    }
+
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create new user
+    const user = new this.userModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    return await user.save();
   }
 }
