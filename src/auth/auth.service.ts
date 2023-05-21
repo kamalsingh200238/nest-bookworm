7598b791b0beee16a999cf702f50e06beb3cdcc6
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-21 18:47:23 +0530

    feat(auth service): add validateUser function to login user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserDocument } from 'src/user/user.schema';
+import { UserService } from 'src/user/user.service';
+import { LogInDto } from './dto/logIn.dto';
+import * as bcrypt from 'bcryptjs';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private userService: UserService,
+  ) {}
 
   async generateJwtToken(user: UserDocument) {
     // payload for jwt token
@@ -13,10 +25,29 @@ export class AuthService {
       username: user.username,
       email: user.email,
     };
-
-    // generate token
-    const token = this.jwtService.sign(payload);
-
+    const token = this.jwtService.sign(payload); // generate token
     return token;
+  }
+
+  async validateUser(logInDto: LogInDto, res: Response) {
+    const { email, password } = logInDto;
+    const user = await this.userService.findByEmail(email);
+    // if the user does not exist
+    if (!user) {
+      throw new NotFoundException('User does not exist');
+    }
+    const passwordMatch = bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Invalid password');
+    }
+    const token = await this.generateJwtToken(user);
+    res.cookie('token', token);
+    res.send({
+      message: 'login successfull',
+      token: token,
+      userId: user._id,
+      username: user.username,
+      email: user.email,
+    });
   }
 }
