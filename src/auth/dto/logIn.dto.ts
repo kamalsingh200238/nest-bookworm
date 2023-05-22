a6ddcc29f1ecf7b067b0159e878c8a9fa57d6a5c
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-22 11:07:11 +0530

    fix(login dto): email should be an email

--- src/auth/dto/logIn.dto.ts
+++ src/auth/dto/logIn.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class LogInDto {
-  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
   @IsNotEmpty()
