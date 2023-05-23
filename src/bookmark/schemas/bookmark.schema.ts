3369985367e6e57f957fb96913989fbf444d9ea3
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-23 18:14:12 +0530

    feat(bookmark.schema.ts): Add bookmark schema for mongoose

---
+++ src/bookmark/schemas/bookmark.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, SchemaTypes} from 'mongoose';
+import { User } from 'src/user/user.schema';
+import { Folder } from './folder.schema';
+
+export type BookmarkDocument = HydratedDocument<Bookmark>;
+
+@Schema()
+export class Bookmark {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  url: string;
+
+  @Prop({ required: true })
+  description: string;
+  
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'User', required: true })
+  user: User;
+
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'Folder', required: true })
+  folder: Folder;
+}
+
+export const BookmarkSchema = SchemaFactory.createForClass(Bookmark);
