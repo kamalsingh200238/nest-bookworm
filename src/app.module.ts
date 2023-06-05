421b38d7736a5b2a68e63a826e3e118f49616031
Kamal Singh <kamalsingh200238@gmail.com>
2023-06-06 02:28:01 +0530

    chore: import bookmark and folder module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { MongooseConfigModule } from './mongoose/mongoose.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { FolderModule } from './folder/folder.module';
 
 @Module({
-  imports: [MongooseConfigModule, UserModule, AuthModule],
+  imports: [MongooseConfigModule, UserModule, AuthModule, BookmarkModule, FolderModule],
   controllers: [AppController],
   providers: [AppService],
 })
