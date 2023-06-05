57aab2e436a3eb3c23cf4056f4dc31d1f7fe8b57
Kamal Singh <kamalsingh200238@gmail.com>
2023-06-06 01:44:56 +0530

    feat: Basic bookmarks and folders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { MongooseConfigModule } from './mongoose/mongoose.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { FoldersModule } from './folders/folders.module';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
 
 @Module({
-  imports: [MongooseConfigModule, UserModule, AuthModule],
+  imports: [
+    MongooseConfigModule,
+    UserModule,
+    AuthModule,
+    FoldersModule,
+    BookmarksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
