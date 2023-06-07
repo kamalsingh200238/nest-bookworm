80f2e4656915730f2d4ed9368bcb135fe4e79945
Kamal Singh <kamalsingh200238@gmail.com>
2023-06-08 00:46:08 +0530

    import folder service in user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { FolderModule } from 'src/folder/folder.module';
 import { User, UserSchema } from './user.schema';
 import { UserService } from './user.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    FolderModule,
   ],
   providers: [UserService],
   exports: [UserService],
