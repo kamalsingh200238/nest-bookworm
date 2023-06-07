c7b524e434af39ccbccf3ff920f3b5e471af18ed
Kamal Singh <kamalsingh200238@gmail.com>
2023-06-07 12:23:23 +0530

    feat: Change folder schema
    - Now schema has a bookmarks field. It is array of bookmark that belong in that folder

--- src/folder/folder.schema.ts
+++ src/folder/folder.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
+import { Bookmark } from 'src/bookmark/bookmark.schema';
 import { User } from 'src/user/user.schema';
 
 export type FolderDocument = mongoose.HydratedDocument<Folder>;
@@ -11,6 +12,12 @@ export class Folder {
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
   user: User;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bookmark' }],
+    required: true,
+  })
+  bookmarks: Bookmark[];
 }
 
 export const FolderSchema = SchemaFactory.createForClass(Folder);
