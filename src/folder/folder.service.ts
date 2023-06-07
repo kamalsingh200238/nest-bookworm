7d9902eef3f48437b0935d93ed7b88ddc3037cd5
Kamal Singh <kamalsingh200238@gmail.com>
2023-06-08 00:45:46 +0530

    feat: function to create default folder

--- src/folder/folder.service.ts
+++ src/folder/folder.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Folder } from './folder.schema';
 
 @Injectable()
-export class FolderService {}
+export class FolderService {
+  constructor(@InjectModel(Folder.name) private folderModel: Model<Folder>) {}
+
+  async createDefaultFolder(userId: Types.ObjectId) {
+    const defaultFolder = await this.folderModel.create({
+      name: 'default',
+      user: userId,
+      bookmarks: [],
+    });
+    return defaultFolder;
+  }
+}
