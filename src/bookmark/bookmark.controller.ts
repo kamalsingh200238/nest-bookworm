73e970778fa42f423ae58aab198943a02213f7ed
Kamal Singh <kamalsingh200238@gmail.com>
2023-06-08 21:11:06 +0530

    feat: Bookmark controller with 2 routes
    - Route to create bookmark
    - Route to get all bookmarks

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { BookmarkService } from './bookmark.service';
+import { CreateBookmarkDto } from './dto/createBookmark.dto';
 
 @Controller('bookmark')
-export class BookmarkController {}
+export class BookmarkController {
+  constructor(private readonly bookmarkService: BookmarkService) {}
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getAllBookmarks(@Req() req: Request) {
+    return this.bookmarkService.getAllBookmarks(req);
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createBookmark(@Body() dto: CreateBookmarkDto, @Req() req: Request) {
+    return this.bookmarkService.createBookmark(dto, req);
+  }
+
+  // @Delete(':id')
+  // @UseGuards(JwtAuthGuard)
+  // async deleteBookmark(@Req() req: Request) {
+  //   return this.bookmarkService.createBookmark();
+  // }
+}
