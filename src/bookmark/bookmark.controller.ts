02d9bd0f59a0ef8d81ee8b3ffac6dc8a104db9c0
Kamal Singh <kamalsingh200238@gmail.com>
2023-06-27 22:02:38 +0530

    new controllers

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,8 +1,20 @@
-import { Body, Controller, Delete, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { Request } from 'express';
+import { Types } from 'mongoose';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { BookmarkService } from './bookmark.service';
 import { CreateBookmarkDto } from './dto/createBookmark.dto';
+import { UpdateBookmarkDto } from './dto/updateBookmark.dto';
 
 @Controller('bookmark')
 export class BookmarkController {
@@ -14,15 +26,31 @@ export class BookmarkController {
     return this.bookmarkService.getAllBookmarks(req);
   }
 
+  // Create a bookmark
   @Post()
   @UseGuards(JwtAuthGuard)
   async createBookmark(@Body() dto: CreateBookmarkDto, @Req() req: Request) {
     return this.bookmarkService.createBookmark(dto, req);
   }
 
-  // @Delete(':id')
-  // @UseGuards(JwtAuthGuard)
-  // async deleteBookmark(@Req() req: Request) {
-  //   return this.bookmarkService.createBookmark();
-  // }
+  // edit a bookmark
+  @Patch('/:bookmarkId')
+  @UseGuards(JwtAuthGuard)
+  async editBookmark(
+    @Param("bookmarkId") bookmarkId: string,
+    @Body() dto: UpdateBookmarkDto,
+    @Req() req: Request,
+  ) {
+    return this.bookmarkService.editBookmark(bookmarkId, dto, req);
+  }
+
+  // delete a bookmark
+  @Delete('/:bookmarkId')
+  @UseGuards(JwtAuthGuard)
+  async deleteBookmark(
+    @Param("bookmarkId") bookmarkId: string,
+    @Req() req: Request,
+  ) {
+    return this.bookmarkService.deleteBookmark(bookmarkId, req);
+  }
 }
